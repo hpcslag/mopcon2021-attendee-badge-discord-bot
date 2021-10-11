5f62e7f2edac82831e042c4c4099975ab2cad02b
Mac Taylor <cslag@hotmail.com.tw>
2021-10-11 13:36:43 +0900

    let record backup file storing time and user

--- go.mod
+++ go.mod
@@ -4,20 +4,37 @@ go 1.17
 
 require (
 	github.com/bwmarrin/discordgo v0.23.2
+	github.com/gin-gonic/gin v1.7.4
+	github.com/robfig/cron/v3 v3.0.0
 	github.com/tealeg/xlsx/v3 v3.2.3
 )
 
 require (
 	github.com/frankban/quicktest v1.5.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/btree v1.0.0 // indirect
-	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/kr/text v0.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/rogpeppe/fastuuid v1.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/shabbyrobe/xmlwriter v0.0.0-20200208144257-9fca06d00ffa // indirect
-	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
-	golang.org/x/text v0.3.2 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"flag"
-	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/robfig/cron/v3"
 )
 
 // before using the bot, please use this url paste to browser and accept it to channel or guild
@@ -12,26 +18,47 @@ import (
 
 // Variables used for command line parameters
 var (
-	BotSecret string
-	GuildID   string
+	BotSecret    string
+	GuildID      string
+	XLSXFile     string
+	BackupPeroid string
 )
 
 func init() {
 
+	// -t token -g gulidid -f test.xlsx
 	flag.StringVar(&BotSecret, "t", "", "Bot Token")
 	flag.StringVar(&GuildID, "g", "", "855718269092233247")
+	flag.StringVar(&XLSXFile, "f", "", "test.xlsx")
+	flag.StringVar(&BackupPeroid, "period", "", "3600")
 	flag.Parse()
 }
 
 func main() {
 
-	ReadXLSXToMap("test.xlsx", &KeyPairMap)
+	ReadXLSXToMap(XLSXFile, &KeyPairMap)
+
+	c := cron.New()
+	c.AddFunc("@every "+BackupPeroid+"s", func() {
+		SaveUsedCSV()
+		log.Println("File Backup Done.")
+	})
+	go c.Start()
+	// close cron
+	defer c.Stop()
 
 	RegisterBotFuncAndRun(DiscordAuth{
 		BotSecret: BotSecret,
 	}, messageCreate)
-
 }
+
+// check the used KKTIX Token.
+type Record struct {
+	User string
+	Time string
+}
+
+var usedToken map[string]*Record = make(map[string]*Record)
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
@@ -56,32 +83,52 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if err != nil {
 		panic(err)
 	}
+
+	// comefrom private messahe
 	if isDM {
-		//					 global guild id 			role id e.g: "885459800740212817" is for attendee
-		s.GuildMemberRoleAdd(GuildID, m.Author.ID, "885459800740212817")
-		s.ChannelMessageSend(m.ChannelID, "Done.")
+
+		// split to command
+		commands := strings.Split(m.Content, " ")
+
+		if len(commands) == 0 {
+			s.ChannelMessageSend(m.ChannelID, "找不到這項指令，若您希望註冊您在 MOPCON 2021 的身分，請您輸入: 「kktix [您的票號]」。")
+			return
+		}
+
+		if commands[0] == "kktix" {
+			// check commands
+			if len(commands) != 2 {
+				s.ChannelMessageSend(m.ChannelID, "您還沒輸入正確的票號喔，您需要輸入: 「kktix [您的票號]」，範例: 「kktix 123456789」。")
+				return
+			}
+
+			// block duplicate register
+			if _, ok := usedToken[commands[1]]; ok {
+				s.ChannelMessageSend(m.ChannelID, "這個票號已經被註冊過了，請尋求服務台頻道的協助。")
+				return
+			}
+
+			// given badge and set used token
+			if badge, ok := KeyPairMap[commands[1]]; ok {
+				usedToken[commands[1]] = &Record{
+					User: m.Author.ID,
+					Time: time.Now().Format("2006-01-02-15:04:05"),
+				}
+
+				s.GuildMemberRoleAdd(GuildID, m.Author.ID, badge)
+
+				s.ChannelMessageSend(m.ChannelID, "您的身分已經完成設定，歡迎您回到 MOPCON Discord 會場!")
+			} else {
+				s.ChannelMessageSend(m.ChannelID, "這個票號不存在，請尋求服務台頻道的協助。")
+				return
+			}
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "這個指令不存在，您需要輸入: 「kktix [您的票號]」，範例: 「kktix 123456789」 以進行註冊。")
+			return
+		}
 	} else {
-
-		if m.Content == "給我身分" {
-			fmt.Println(m.GuildID, m.Author.ID)
-			s.GuildMemberRoleAdd(m.GuildID, m.Author.ID, "885459800740212817")
-
-			s.ChannelMessageSend(m.ChannelID, "好")
-		}
-
-		if badge, ok := KeyPairMap[m.Content]; ok {
-			s.ChannelMessageSend(m.ChannelID, "Badge: "+badge)
-		}
-
-		// If the message is "ping" reply with "Pong!"
-		if m.Content == "ping" {
-			s.ChannelMessageSend(m.ChannelID, "Pong!")
-		}
-
-		// If the message is "pong" reply with "Ping!"
-		if m.Content == "pong" {
-			s.ChannelMessageSend(m.ChannelID, "Ping!")
-		}
+		// receive global message
+		s.ChannelMessageSend(m.ChannelID, "請您使用私訊的方式進行 KKTIX 驗票註冊，謝謝您!")
 	}
 }
 
@@ -94,4 +141,22 @@ func ComesFromDM(s *discordgo.Session, m *discordgo.MessageCreate) (bool, error)
 	}
 
 	return channel.Type == discordgo.ChannelTypeDM, nil
+}
+
+func SaveUsedCSV() {
+
+	csvData := [][]string{
+		{"code", "user", "time"},
+	}
+	for key, val := range usedToken {
+		csvData = append(csvData, []string{key, val.User, val.Time})
+	}
+
+	b := new(bytes.Buffer)
+	w := csv.NewWriter(b)
+	w.WriteAll(csvData)
+
+	t := time.Now()
+	os.WriteFile(t.Format("20060102150405")+".csv", b.Bytes(), 0644)
+
 }
